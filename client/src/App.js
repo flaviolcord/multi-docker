c2eefe65bec4469d64a6dc29d39b8f3d7c7fba5a
flaviolcord <flaviolucas197@gmail.com>
2024-10-29 22:47:15 +0100

    headline modification home page

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Fibonacci Values
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
